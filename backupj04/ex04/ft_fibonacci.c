822417e29a987f4a1b8c8dc39c8560db74db0664
Leo CABANES <leocabanes@wanadoo.fr>
2017-07-10 09:20:33 +0200

    Ajout des ex 00 a 05 du j04

---
+++ backupj04/ex04/ft_fibonacci.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcabanes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/10 08:29:15 by lcabanes          #+#    #+#             */
+/*   Updated: 2017/07/10 08:35:32 by lcabanes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_fibonacci(int index)
+{
+	if (index < 0)
+	{
+		return (-1);
+	}
+	else if (index == 0)
+	{
+		return (0);
+	}
+	else if (index == 1)
+	{
+		return (1);
+	}
+	else
+	{
+		return (ft_fibonnacci(index - 1) + ft_fibonacci(index - 2));
+	}
+}

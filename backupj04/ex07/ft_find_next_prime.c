e0f70ecc6582e6bc2387ad3cf7b79a0d4eb298bd
Leo CABANES <leocabanes@wanadoo.fr>
2017-07-11 04:43:30 +0200

    Ajout ex07

---
+++ backupj04/ex07/ft_find_next_prime.c
@@ -0,0 +1,52 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lcabanes <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/07/10 10:43:46 by lcabanes          #+#    #+#             */
+/*   Updated: 2017/07/11 04:40:04 by lcabanes         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_is_prime(int nb)
+{
+	int i;
+	int a;
+
+	i = 2;
+	a = nb / 2;
+	while (i < a)
+	{
+		if (a * i == nb)
+		{
+			return (0);
+		}
+		i++;
+		a = nb / i;
+	}
+	return (1);
+}
+
+void	aux_ft_find_next_prime(int *nb)
+{
+	if (ft_is_prime(*nb) == 0)
+	{
+		*nb = *nb + 1;
+		aux_ft_find_next_prime(nb);
+	}
+	else
+	{
+	}
+}
+
+int		ft_find_next_prime(int nb)
+{
+	if (nb <= 2)
+	{
+		return (2);
+	}
+	aux_ft_find_next_prime(&nb);
+	return (nb);
+}
